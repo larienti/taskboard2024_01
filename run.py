59477e2e762df1be0746975b75da86f4b32c31c9
Lars Arienti <larsa@larss-mbp.myfiosgateway.com>
2024-07-22 15:18:36 -0400

    fixes

--- run.py
+++ run.py
@@ -2,7 +2,7 @@ from app import create_app, db
 from flask_migrate import Migrate
 
 app = create_app()
-migrate = Migrate(app, db)
+migrate = Migrate(app, db, directory=os.path.join(os.path.dirname(__file__), 'migrations'))
 
 if __name__ == '__main__':
     app.run()
