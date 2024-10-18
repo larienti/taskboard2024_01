05b317ec85cf7e2392cba75331ace1aa981c2cb4
Lars Arienti <larsa@larss-mbp.myfiosgateway.com>
2024-10-18 14:54:52 -0400

    all

--- config.py
+++ config.py
@@ -3,11 +3,11 @@ from urllib.parse import urlparse, parse_qsl, urlencode
 
 class Config:
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'dev_secret_key'
-    db_url = os.environ.get('DATABASE_URL')
+    db_url = os.environ.get('DATABASE_URL') or os.environ.get('DATABASE_PUBLIC_URL')
     if db_url:
         url = urlparse(db_url)
         query = dict(parse_qsl(url.query))
-        query.update({'sslmode': 'require', 'connect_timeout': '30'})
+        query.update({'connect_timeout': '30'})
         url = url._replace(query=urlencode(query))
         SQLALCHEMY_DATABASE_URI = url.geturl()
     else:
