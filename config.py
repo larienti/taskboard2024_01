165098cd330a9df9c5395742541d98e5b2bd1067
Lars Arienti <larsa@larss-mbp.myfiosgateway.com>
2024-10-18 13:36:46 -0400

    close?

--- config.py
+++ config.py
@@ -1,10 +1,17 @@
 import os
+from urllib.parse import urlparse, urlencode
 
 class Config:
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'dev_secret_key'
-    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_PUBLIC_URL')
-    if SQLALCHEMY_DATABASE_URI and SQLALCHEMY_DATABASE_URI.startswith("postgres://"):
-        SQLALCHEMY_DATABASE_URI = SQLALCHEMY_DATABASE_URI.replace("postgres://", "postgresql://", 1)
+    db_url = os.environ.get('DATABASE_PUBLIC_URL')
+    if db_url:
+        url = urlparse(db_url)
+        query = dict(urlparse.parse_qsl(url.query))
+        query.update({'connect_timeout': '10'})
+        url = url._replace(query=urlencode(query))
+        SQLALCHEMY_DATABASE_URI = url.geturl()
+    else:
+        SQLALCHEMY_DATABASE_URI = 'sqlite:///your_database.db'  # Fallback to SQLite
     SQLALCHEMY_TRACK_MODIFICATIONS = False
 
 class DevelopmentConfig(Config):
