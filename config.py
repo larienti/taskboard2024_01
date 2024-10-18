ebb172fdf61e1eea5a91170933e94ee51b2288fb
Lars Arienti <larsa@larss-mbp.myfiosgateway.com>
2024-10-18 13:18:48 -0400

    change back to nonpublic db in config

--- config.py
+++ config.py
@@ -2,7 +2,7 @@ import os
 
 class Config:
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'dev_secret_key'
-    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_PUBLIC_URL')
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL')
     if SQLALCHEMY_DATABASE_URI and SQLALCHEMY_DATABASE_URI.startswith("postgres://"):
         SQLALCHEMY_DATABASE_URI = SQLALCHEMY_DATABASE_URI.replace("postgres://", "postgresql://", 1)
     SQLALCHEMY_TRACK_MODIFICATIONS = False
