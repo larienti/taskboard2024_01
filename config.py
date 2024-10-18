8f194bdaf5b5114598855c541f62af4b7aa3140c
Lars Arienti <larsa@larss-mbp.myfiosgateway.com>
2024-10-18 13:39:01 -0400

    whoops

--- config.py
+++ config.py
@@ -1,12 +1,12 @@
 import os
-from urllib.parse import urlparse, urlencode
+from urllib.parse import urlparse, parse_qsl, urlencode
 
 class Config:
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'dev_secret_key'
     db_url = os.environ.get('DATABASE_PUBLIC_URL')
     if db_url:
         url = urlparse(db_url)
-        query = dict(urlparse.parse_qsl(url.query))
+        query = dict(parse_qsl(url.query))
         query.update({'connect_timeout': '10'})
         url = url._replace(query=urlencode(query))
         SQLALCHEMY_DATABASE_URI = url.geturl()
