c6d8c394817892223cbee12ca928f9ca04f3cc47
Lars Arienti <larsa@larss-mbp.myfiosgateway.com>
2024-11-09 13:22:56 -0500

    nothing

--- config.py
+++ config.py
@@ -2,7 +2,7 @@ import os
 
 class Config:
     SECRET_KEY = os.getenv('SECRET_KEY', 'default_dev_key')
-    SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URL', 'sqlite:///default.db')
+    SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URL')  # Use DATABASE_URL from the environment
     SQLALCHEMY_TRACK_MODIFICATIONS = False
 
 class DevelopmentConfig(Config):
